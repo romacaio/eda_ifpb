63a4f8009fb67cde2b5d9725033c181341469afa
Caio Romão <caiodev14@gmail.com>
2025-11-26 21:36:18 -0300

    feat: Pesquisa Sequencial Com Transposição de Vários

---
+++ eda/src/pesquisa/PesquisaSequencialTransposicaoVarios.java
@@ -0,0 +1,38 @@
+package pesquisa;
+
+public class PesquisaSequencialTransposicaoVarios {
+    public static void main(String[] args) {
+
+        int[] array = {10, 20, 30, 40, 50};
+        int valor = 40;
+        int posicoes = 2;
+
+        int resultado = PesquisaSequencial(array, valor, posicoes);
+        if (resultado != -1) {
+            System.out.println("Valor encontrado e transposto.");
+            System.out.print("Array depois da transposição: ");
+            for (int num : array) {
+                System.out.print(num + " ");
+            }
+        } else {
+            System.out.println("Valor não econtrado.");
+        }
+
+    }
+
+    public static int PesquisaSequencial(int[] array, int valor, int posicoes) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == valor) {
+                int novaPosicao = Math.max(0, i - posicoes);
+                int temp = array[i];
+
+                for (int j = i; j > novaPosicao; j--) {
+                    array[j] = array[j - 1];
+                }
+                array[novaPosicao] = temp;
+                return novaPosicao;
+            }
+        }
+        return -1;
+    }
+}
